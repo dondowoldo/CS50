1b6baaa6bc5e9847b7724e549fa4ba3905760a52
Daniel Valasek <daniel.valasek@yahoo.com>
2023-05-12 23:29:35 +0100

    initial design setup

--- Final_Project/booking/urls.py
+++ Final_Project/booking/urls.py
@@ -6,5 +6,6 @@ app_name = "booking"
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
 ]
