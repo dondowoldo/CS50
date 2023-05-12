a1c01bc948166bedad6138ff4fbdf38e032d6166
dondowoldo <daniel.valasek@yahoo.com>
2023-05-12 02:07:57 +0200

    register implemented

--- Final_Project/booking/urls.py
+++ Final_Project/booking/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = "booking"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("register", views.register, name="register")
 ]
--- Final_Project/booking/views.py
+++ Final_Project/booking/views.py
@@ -19,7 +19,7 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("booking:index"))
         else:
             return render(request, "booking/login.html", {
                 "message": "Invalid username and/or password."
@@ -30,37 +30,41 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index"))
+    return HttpResponseRedirect(reverse("booking:index"))
 
 
 def register(request):
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
 
         # Ensure password matches confirmation
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "auctions/register.html", {
+            return render(request, "booking/register.html", {
                 "message": "Passwords must match."
             })
 
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
+            user.first_name = first_name
+            user.last_name = last_name
             user.save()
         except IntegrityError:
-            return render(request, "auctions/register.html", {
+            return render(request, "booking/register.html", {
                 "message": "Username already taken."
             })
         login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+        return HttpResponseRedirect(reverse("booking:index"))
     else:
-        return render(request, "auctions/register.html")
+        return render(request, "booking/register.html")
 
 
 def index(request):
     return render(request, "booking/index.html", {
-        
+
     })
