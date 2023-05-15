74c5f08af3e98cb07a1379e7b4f3f03d51ecc634
Daniel Valasek <daniel.valasek@yahoo.com>
2023-05-15 13:52:06 +0100

    model update

--- Final_Project/booking/admin.py
+++ Final_Project/booking/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Listing, Comment, Message
 
 class UserAdmin(admin.ModelAdmin):
     list_display = "id", "username", "first_name", "last_name", "email","date_joined", "is_staff"
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = "id", "name", "creator", "price_per_day", "property_type", "date_created", "active"
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = "user", "listing", "comment", "timestamp"
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = "sent_from", "sent_to", "message", "timestamp"
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = "user", "listing", "total_price", "duration", "timestamp"
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Message, MessageAdmin)
--- Final_Project/booking/models.py
+++ Final_Project/booking/models.py
@@ -4,3 +4,49 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     def __str__(self):
         return f"{self.username}"
+    
+class Listing(models.Model):
+    name = models.CharField(max_length=100)
+    price_per_day = models.FloatField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created")
+    date_created = models.DateTimeField(auto_now_add=True)
+    property_type = models.CharField(max_length=10)
+    active = models.BooleanField(default=True)
+    imageurl = models.URLField(max_length=300, blank=True)
+    location = models.CharField(max_length=30)
+    gps_location = models.CharField(max_length=50)
+    watchlist = models.ManyToManyField(User, blank=True, related_name="watching")
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class Comment(models.Model):
+    comment = models.TextField(max_length=300, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="user")
+    listing = models.ForeignKey(Listing, null=True, on_delete=models.CASCADE, related_name="listing")
+    timestamp = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f"{self.comment}"
+    
+
+class Message(models.Model):
+    sent_from = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
+    sent_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f"{self.sent_from} : {self.sent_to} : {self.timestamp} : {self.message}"
+    
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="customer")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="property")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    duration = models.DurationField()
+    total_price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.user} : {self.listing}, {self.timestamp}, {self.duration}, {self.total_price}"
